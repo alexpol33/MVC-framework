3f3ca6bfb9892a2484c7c116d679fb2f5b51572b
Alexpol <alexpol2905@gmail.com>
2021-08-13 11:10:06 +0300

    add routes

---
+++ application/config/routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return[
+    'test' => [
+        'controller' => 'account',
+        'action' => 'login'
+        ],
+    'news/show' =>[
+        'controller' => 'news',
+        'action' => 'show'
+    ]
+];
