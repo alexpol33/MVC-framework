663ae09c4d968853fa79de68141bf5ed7947b6d1
Alexpol <alexpol2905@gmail.com>
2021-08-16 15:57:33 +0300

    protection against sql injections

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -10,8 +10,15 @@ class MainController extends Controller
 
         public function indexAction(){
             $db = new Db;
-            $data = $db->row('SELECT full_name from users');
+
+            $params = [
+                'id' => 2,
+            ];
+
+            $data = $db->column('SELECT full_name from users where id = :id', $params);
+
             debug($data);
+
             $this->view->render('Главная страница');
             //echo 'Главная Страница';
         }
