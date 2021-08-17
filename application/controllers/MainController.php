c0b1a091d01ea6e348e651f16f0d8a52cffca379
Alexpol <alexpol2905@gmail.com>
2021-08-17 23:05:36 +0300

    class Model, main model

--- application/controllers/MainController.php
+++ application/controllers/MainController.php
@@ -5,22 +5,16 @@ namespace application\controllers;
 use application\core\Controller;
 use application\lib\Db;
 
+
 class MainController extends Controller
 {
 
         public function indexAction(){
-            $db = new Db;
-
-            $params = [
-                'id' => 2,
+            $result = $this->model->getNews();
+            $vars = [
+                'news' => $result
             ];
-
-            $data = $db->column('SELECT full_name from users where id = :id', $params);
-
-            debug($data);
-
-            $this->view->render('Главная страница');
-            //echo 'Главная Страница';
+            $this->view->render('Главная страница', $vars);
         }
 
 
---
+++ application/models/Main.php
@@ -0,0 +1,14 @@
+<?php
+namespace application\models;
+
+use application\core\Model;
+use application\lib\Db;
+
+class Main extends Model
+{
+
+    public function getNews(){
+        return $this->db->row('select * from posts');
+    }
+
+}
